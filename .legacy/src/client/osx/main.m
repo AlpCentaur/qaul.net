c7ccb9fd7590982d802f18fdc3d286e2f4109f3a
Katharina Fey <kookie@spacekookie.de>
2018-02-14 23:58:56 +0100

    Adding the legacy code directory for colaboration

---
+++ .legacy/src/client/osx/main.m
@@ -0,0 +1,11 @@
+/*
+ * qaul.net is free software
+ * licensed under GPL (version 3)
+ */
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
